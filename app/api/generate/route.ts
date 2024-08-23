65b0382161df3a2f15ccc56b8a0df4fcbf4c786e
Oleksii Butrin <bootreen@gmail.com>
2024-08-23 13:11:59 +0200

    Added basic Gemini API request

---
+++ app/api/generate/route.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+import events from "events";
+
+import {
+  GoogleGenerativeAI,
+  GenerateContentResult,
+} from "@google/generative-ai";
+import { NextResponse } from "next/server";
+
+interface RequestData {
+  body: string;
+}
+
+// Walkaround for free VPN usage
+events.EventEmitter.defaultMaxListeners = 20;
+
+let genAI: GoogleGenerativeAI | null = null;
+
+export async function POST(req: Request): Promise<NextResponse> {
+  try {
+    if (!genAI) {
+      genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+    }
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const data: RequestData = await req.json();
+    const prompt = data.body;
+    const result: GenerateContentResult = await model.generateContent(prompt);
+    const output = result.response.text() || "No content available";
+
+    console.log(result);
+
+    return NextResponse.json({ output });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 "use client";
 
 import { Textarea } from "@nextui-org/input";
@@ -13,8 +14,34 @@ const Home = () => {
   const { checkboxes, request } = useAppStates((state) => state);
   const { setCheckboxState, setRequestContent } = useAppActions();
 
-  // eslint-disable-next-line no-console
-  console.log(request);
+  const sendRequest = async () => {
+    try {
+      // use the fetch method to send an http request to /api/generate endpoint
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ body: request }),
+      });
+
+      // Waits for the response to be converted to JSON format and stores it in the data variable
+      const data = await response.json();
+
+      //  If successful, updates the output state with the output field from the response data
+      if (response.ok) {
+        console.log(data);
+      } else {
+        console.log(data.error);
+      }
+
+      // Catches any errors that occur during the fetch request
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  // console.log(request);
 
   const onCheckboxToggle = (
     checkbox: keyof typeof checkboxes,
@@ -61,6 +88,7 @@ const Home = () => {
           color="primary"
           radius="sm"
           size="lg"
+          onPress={() => sendRequest()}
         >
           Generate
         </Button>
