a56df5792bfdf6d01ea9daece9d3573e71781645
Oleksii Butrin <bootreen@gmail.com>
2024-08-29 17:03:00 +0200

    Updated prompt for other instructions

--- config/prompt.ts
+++ config/prompt.ts
@@ -10,18 +10,6 @@ const promptParams = {
   guideLength: 1200,
 };
 
-// export const requestInstructions = {
-//   prefix: `Please create comprehensive learning resources on the above topic, including the following components:\n`,
-//   guide: `- Guide (a concise step-by-step introduction to the topic, consisting of ${promptParams.stepsMin} to ${promptParams.stepsMax} steps, with the number of steps depending on the topic's complexity)\n`,
-//   summary: `- Summary (a detailed explanation of the topic, ${promptParams.guideLength} symbols max)\n`,
-//   flashcards: `- Flashcards (${promptParams.flashcards} cards with key terms and their definitions), content length limit both for question and answer is ${promptParams.flashcardLength} symbols\n`,
-//   pairmatch: `- (${promptParams.pairs} matching pairs related to the topic, with a content length limit of ${promptParams.pairLength} symbols for each part of the pair. No elements in the pairs should be repeated. Create pairs where even the incorrect matches appear plausible and do not seem absurd or obviously wrong at first glance. These pairs should create a sense of doubt and require thoughtful consideration. All items should be related to a single theme or part of the given topic so that even the incorrect matches could potentially seem correct, requiring careful evaluation to identify the right answers. If it is absolutely impossible to generate pairs strictly following the above instructions, return an empty pairmatch array.)\n`,
-//   quiz: `- Quiz (${promptParams.questions} multiple-choice questions with ${promptParams.options} answer options each)\n`,
-//   subtopics: `- Subtopics (Suggest potential subtopics for further exploration if the topic is too large and complex; provide an empty array if the main topic doesn't need to be divided into subtopics)\n`,
-//   note: `Re-word the topic to remove errors and reduce its length to 5 words maximum. Different types of learning materials should not 100%-duplicate each other but can be thematically interconnected and loosely based on each other.\nEmbrace all multi-line code snippets with triple plus (+++); avoid using double quotes (") inside code snippets; if this is inevitable, escape them with a backslash (\\).\nAll formulae must be presented strictly in VALID KaTeX format!\nIf you cannot execute a request correctly (for example, because the input query is too short or unclear), generate an error with an explanatory message (see the error response schema).\nIf the requested topic is too large and extensive, also display an error and provide a list of subtopics you propose to split it into.`,
-//   postfix: `SUPER IMPORTANT! Ensure that the output JSON is valid, and fix syntax if needed. Provide the output in JSON strictly using the following schema:\n`,
-// };
-
 export const requestInstructions = {
   prefix: `Please create comprehensive learning resources on the topic provided above, including the following components:\n`,
 
