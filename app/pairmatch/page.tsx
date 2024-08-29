57e1ea637646c2ff7269ec6550707180e06f24f6
Oleksii Butrin <bootreen@gmail.com>
2024-08-29 16:59:20 +0200

    Added skeleton for pairmatch tab, improved pairs prompt

--- app/pairmatch/page.tsx
+++ app/pairmatch/page.tsx
@@ -1,7 +1,43 @@
-const PairsPage = () => (
-  <div>
-    <h1>Flashcards tab</h1>
-  </div>
-);
+"use client";
+
+import { Button } from "@nextui-org/button";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/table";
+
+import { useAppStates, useAppActions } from "@/store/app-states";
+import MarkdownRenderer from "@/utils/md-renderer";
+import common from "@/styles/page.default.module.css";
+
+const PairsPage = () => {
+  const { topic, pairmatch } = useAppStates((state) => state);
+  const {} = useAppActions();
+
+  return (
+    <article className={common.container}>
+      <h2>{topic}: Pair match</h2>
+      <Table hideHeader aria-label="Matching pairs">
+        <TableHeader>
+          <TableColumn>Q</TableColumn>
+          <TableColumn>A</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {pairmatch.map((pair, i) => (
+            <TableRow key={i}>
+              <TableCell>{pair.question}</TableCell>
+              <TableCell>{pair.answer}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </article>
+  );
+};
 
 export default PairsPage;
